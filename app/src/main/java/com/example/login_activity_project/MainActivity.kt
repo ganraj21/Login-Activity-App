df8399293e1096a2f630c8e953b65cff725ba7ff
Ganesh Ghadge <89139437+ganraj21@users.noreply.github.com>
2022-10-20 01:22:28 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/login_activity_project/MainActivity.kt
+++ app/src/main/java/com/example/login_activity_project/MainActivity.kt
@@ -1,22 +1,57 @@
-package com.example.login_activity_project
+package com.example.my_login_app
 
+import android.content.Context
+import android.graphics.fonts.FontFamily
+import android.graphics.fonts.FontStyle
+import android.media.Image
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.MagnifierStyle.Companion.Default
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle.Companion.Italic
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.login_activity_project.ui.theme.Login_activity_projectTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.my_login_app.ui.theme.My_login_appTheme
 
 class MainActivity : ComponentActivity() {
-    fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Login_activity_projectTheme {
+            My_login_appTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
                     LoginUI()
                 }
             }
@@ -26,76 +61,139 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LoginUI() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(20.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
 
-        var email by remember { mutableStateOf(TextFieldValue("")) }
-        var password by remember { mutableStateOf(TextFieldValue("")) }
-
-
-        Text(
-            text = "Login Here",
-            textAlign = TextAlign.Left,
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Blue,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp)
+    Box() {
+        val image = painterResource(R.drawable.login_image)
+        Image(
+            painter = image,
+            contentDescription = null
         )
-
-        OutlinedTextField(
-            value = email,
-            onValueChange = {
-                email = it
-            },
-            label = { Text(text = "Enter Your Name") },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Person,
-                    contentDescription = "person"
-                )
-            },
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 10.dp, top = 10.dp)
-
-        )
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = {
-                password = it
-            },
-            label = { Text(text = "Enter Your password") },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Info,
-                    contentDescription = "password"
-                )
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp, top = 10.dp),
-            visualTransformation = PasswordVisualTransformation(),
-//            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-        )
-        val context = LocalContext.current
-        Button(
-            onClick = { logged(email.toString(), password.toString(), context) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp, top = 10.dp)
-                .height(50.dp)
+                .padding(top = 200.dp)
         ) {
-            Text(text = "Login", textAlign = TextAlign.Center,fontSize = 20.sp)
-        }
 
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                var email by remember { mutableStateOf(TextFieldValue("")) }
+                var password by remember { mutableStateOf(TextFieldValue("")) }
+
+
+                Text(
+                    text = "Login Here",
+                    textAlign = TextAlign.Left,
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Blue,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                )
+
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = {
+                        email = it
+                    },
+                    label = { Text(text = "Enter Your Name") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = "person"
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 10.dp, top = 10.dp)
+
+                )
+
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = {
+                        password = it
+                    },
+                    label = { Text(text = "Enter Your password") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Info,
+                            contentDescription = "password"
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 10.dp, top = 10.dp),
+                    visualTransformation = PasswordVisualTransformation(),
+//            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                )
+                val context = LocalContext.current
+                Button(
+                    onClick = { logged(email.toString(), password.toString(), context) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 10.dp, top = 10.dp)
+                        .height(50.dp)
+                ) {
+                    Text(text = "Login", textAlign = TextAlign.Center, fontSize = 20.sp)
+                }
+
+                ClickableText(
+                    text = AnnotatedString("Forgot password?"),
+                    onClick = { },
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        textAlign = TextAlign.Center,
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                )
+
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp)) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .width(350.dp)
+                            .height(70.dp)
+                            .padding(10.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
+                    ) {
+                        Text(
+                            text = "Sign in with Google+",
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            fontSize = 20.sp,
+                            color = Color.White
+                        )
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .width(350.dp)
+                            .height(70.dp)
+                            .padding(10.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
+                    ) {
+                        Text(
+                            text = "Login With Facebook",
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            fontSize = 20.sp,
+                            color = Color.Blue
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -103,16 +201,16 @@ fun logged(email: String, password: String, context: Context) {
 
     if ((email == "ganraj") && (password == "1234")) {
         Toast.makeText(context, "Logged In Successfully", Toast.LENGTH_SHORT).show()
+
     } else {
         Toast.makeText(context, "Logged In failed", Toast.LENGTH_SHORT).show()
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Login_activity_projectTheme {
+    My_login_appTheme {
         LoginUI()
     }
 }
