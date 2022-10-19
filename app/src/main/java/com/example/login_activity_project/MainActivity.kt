c0735f60ab49d10b3acf273e87a63815ad6d52d7
Ganesh Ghadge <89139437+ganraj21@users.noreply.github.com>
2022-10-19 23:15:13 +0530

    Update MainActivity.kt
    change button

--- app/src/main/java/com/example/login_activity_project/MainActivity.kt
+++ app/src/main/java/com/example/login_activity_project/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.login_activity_project.ui.theme.Login_activity_projectTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Login_activity_projectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LoginUI()
                 }
             }
         }
@@ -25,14 +25,94 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LoginUI() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        var email by remember { mutableStateOf(TextFieldValue("")) }
+        var password by remember { mutableStateOf(TextFieldValue("")) }
+
+
+        Text(
+            text = "Login Here",
+            textAlign = TextAlign.Left,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        )
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = { Text(text = "Enter Your Name") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "person"
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp)
+
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = { Text(text = "Enter Your password") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Info,
+                    contentDescription = "password"
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp),
+            visualTransformation = PasswordVisualTransformation(),
+//            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+        val context = LocalContext.current
+        Button(
+            onClick = { logged(email.toString(), password.toString(), context) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp)
+                .height(50.dp)
+        ) {
+            Text(text = "Login", textAlign = TextAlign.Center,fontSize = 20.sp)
+        }
+
+    }
 }
+
+fun logged(email: String, password: String, context: Context) {
+
+    if ((email == "ganraj") && (password == "1234")) {
+        Toast.makeText(context, "Logged In Successfully", Toast.LENGTH_SHORT).show()
+    } else {
+        Toast.makeText(context, "Logged In failed", Toast.LENGTH_SHORT).show()
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Login_activity_projectTheme {
-        Greeting("Android")
+        LoginUI()
     }
 }
